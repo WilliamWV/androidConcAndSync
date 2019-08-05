556dd547d7c9f5114a0dc1e9d1f07736222d6b2c
WilliamWV <william.w.vargas@gmail.com>
2019-08-05 10:35:29 -0300

    Implemented download of file with AsyncTask

---
+++ app/src/main/java/com/example/concurrencyeval/implementations/download/DwAsyncTask.kt
@@ -0,0 +1,36 @@
+package com.example.concurrencyeval.implementations.download
+
+import android.os.AsyncTask
+import com.example.concurrencyeval.Constants
+import com.example.concurrencyeval.activities.FileDownloadActivity
+import com.example.concurrencyeval.util.RunReport
+import java.io.File
+import java.io.FileOutputStream
+import java.net.URL
+import kotlin.system.measureTimeMillis
+
+class DwAsyncTask: AsyncTask<FileDownloadActivity, Void, Void>() {
+
+
+    fun String.saveTo(file: File) {
+        URL(this).openStream().use { input ->
+            FileOutputStream(file).use { output ->
+                input.copyTo(output)
+            }
+        }
+    }
+    override fun doInBackground(vararg activities: FileDownloadActivity?): Void? {
+        val time = measureTimeMillis {
+            val dir: File = activities[0]!!.baseContext.cacheDir
+            val file: File = File.createTempFile("temp_book", "pdf", dir)
+            Constants.DOWNLOAD_FILE_87_LINK.saveTo(file)
+            file.delete()
+        }
+
+        activities[0]?.runOnUiThread{
+            activities[0]?.updateReport(RunReport(time))
+        }
+        return null
+    }
+
+}
