6d22fa4c33dbe17826ed599007b9833628ace8ac
WilliamWV <william.w.vargas@gmail.com>
2019-09-25 17:32:18 -0300

    Added TestManager class to centralize instrumental tests

---
+++ app/src/androidTest/java/com/example/concurrencyeval/TestManager.kt
@@ -0,0 +1,24 @@
+package com.example.concurrencyeval
+
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class TestManager {
+
+    private val mmTests = FinalMMTest()
+    private val csTests = FinalSumTest()
+    private val phTests = FinalPhTest()
+    private val idTests = FinalImgDownTest()
+    private val pcTests = FinalProdConsTest()
+
+    @Test
+    fun allTests(){
+        mmTests.runTest()
+        csTests.runTest()
+        phTests.runTest()
+        idTests.runTest()
+        pcTests.runTest()
+    }
+}
