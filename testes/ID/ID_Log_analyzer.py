0bf064d72bd092f8d875c86fc099ecf97ce893a9
WilliamWV <william.w.vargas@gmail.com>
2019-10-04 11:51:49 -0300

    Added scripts, logs and spreadsheets to image download problem

---
+++ testes/ID/ID_Log_analyzer.py
@@ -0,0 +1,66 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Receive files to analyze')
+
+parser.add_argument('-f', "--file", required=True, type=str,
+                        help="base name of the log file, ex: \"J5_Log\" to \"J5_Log_01\" ")
+						
+args = parser.parse_args()
+
+
+out = open(args.file.replace('.txt', '') + '_agreggated.csv', 'w')
+
+
+files = os.listdir('.')
+log_files = []
+for file in files:
+	if file.find(args.file) >= 0 and file.find('processed.txt') >=0:
+		log_files.append(file)
+		
+		
+logs = {}
+
+for file in log_files:
+		inp = open(file, 'r')
+
+		previousLine = ''
+		for line in inp.readlines():
+			
+			if previousLine.find('Repetition') != -1 and line.find('Time') != -1:
+				# Log válido
+				img = previousLine[previousLine.find('downloading image of a:') + 24:len(previousLine) - 1]
+				impl = previousLine[previousLine.find("of") + 3:previousLine.find('implementation')-1]
+				time = int(line[line.find('Time') + 6:line.find('ms')-1])
+				
+				if logs.get(impl) == None:
+					logs[impl] = {}
+				if logs[impl].get(img) == None:
+					logs[impl][img] = []
+				logs[impl][img].append(time)
+				
+			previousLine = line
+		
+out.write(';Image download;\n')
+imgs = ["Cat", "Dog", "Lion", "Platypus", "Pigeon"]
+impls = ['Threads', 'ThreadPool', 'HaMeR framework', 'Kotlin coroutines', 'AsyncTask', 'IntentServices']
+repetitions = 32
+
+for img in imgs:
+	out.write(str(img) + ';\n')
+	out.write('\n;;')
+
+	for impl in impls:
+		out.write(str(impl)+';')
+	out.write(';\n')
+	for r in range(repetitions):
+		out.write(';;')
+		for impl in impls:
+			if (len(logs[impl][img]) > r):
+				out.write(str(logs[impl][img][r])+';')
+			else:
+				out.write(';')
+		out.write(';\n')
+	out.write(';\n;\n;\n')
+
+
